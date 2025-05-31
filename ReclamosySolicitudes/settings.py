92688998e900693f527497793bf3ec9006049e08
María Victoria <maria.arteagare@campusucc.edu.co>
2025-05-30 19:34:42 -0500

    Configurar PostgreSQL en producción

--- ReclamosySolicitudes/settings.py
+++ ReclamosySolicitudes/settings.py
@@ -90,13 +90,13 @@ WSGI_APPLICATION = 'ReclamosySolicitudes.wsgi.application'
 # Detecta si estás en Render
 RENDER = os.environ.get('RENDER', False)
 import dj_database_url
-
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'), 
+        conn_max_age=600,         
+        ssl_require=True                    
     )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
